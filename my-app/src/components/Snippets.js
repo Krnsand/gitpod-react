79d0dd4d55feef4211202889fafdd520302c4fb7
Krnsand <krnsandin@gmail.com>
2023-04-22 10:03:16 +0000

    Learn about ES7 spippets

---
+++ my-app/src/components/Snippets.js
@@ -0,0 +1,63 @@
+// rfce =
+
+// import React from 'react'
+
+// function Snippets() {
+//  return (
+//    <div>Snippets</div>
+//  )
+// }
+
+// export default Snippets
+
+// ---------------------------------------------
+
+// rafce =
+
+// import React from 'react'
+
+// const Snippets = () => {
+//  return (
+//    <div>Snippets</div>
+//  )
+// }
+
+// export default Snippets
+
+// ---------------------------------------------
+
+// rce =
+
+// import React, { Component } from 'react'
+
+// export class Snippets extends Component {
+//  render() {
+//    return (
+//      <div>Snippets</div>
+//    )
+//  }
+// }
+
+// export default Snippets
+
+// ----------------------------------------------
+
+// rconst =
+
+// constructor(props) {
+//  super(props)
+
+//  this.state = {
+//     first
+//  }
+// }
+
+// ----------------------------------------------
+
+// rcg =
+
+// console.log(first)
+
+// rco =
+
+// console.log('first', first)
