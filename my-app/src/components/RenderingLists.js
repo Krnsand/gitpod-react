1408837ff8e2263a7c71ca693154aa5c3b4cf0b1
Krnsand <krnsandin@gmail.com>
2023-04-22 13:43:13 +0000

    Learn to render lists

---
+++ my-app/src/components/RenderingLists.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Book from './Book'
+
+function RenderingLists() {
+    const bookList = [
+        "To Kill A Mockingbird",  
+        "The Great Gatsby",
+        "The Catcher In The Rye",
+    ]
+
+    const books = [
+        {
+            title: "To Kill A Mockingbird", 
+            author: "Harper Lee",
+            pages: 281
+        },
+        {
+            title: "The Great Gatsby", 
+            author: "F. Scott Fitzgerald",
+            pages: 218
+        },
+        {
+            title: "The Catcher In The Rye", 
+            author: "J. D. Salinger",
+            pages: 234
+        }
+
+    ]
+
+  return (
+    <div>
+        {bookList.map(book => {
+            return <h2>{book}</h2>
+        })}
+        <hr />
+        {
+            books.map(book => {
+                return (
+                    <div>
+                        <h5>{book.title}</h5>
+                        <p>{book.author}</p>
+                        <p>{book.pages}</p>
+                    </div>
+                )
+            })
+        }
+        <hr />
+        {
+            books.map(book => {
+                return <Book book={book} />
+            })
+        }
+    </div>
+  )
+}
+
+export default RenderingLists
